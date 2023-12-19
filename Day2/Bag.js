03473fc489844ac53e4b711595d430794db5286d
jasper.jackson <jasper.jackson@madetech.com>
2023-12-19 18:06:22 +0000

    Added test for multiple rounds

--- Day2/Bag.js
+++ Day2/Bag.js
@@ -1,10 +1,17 @@
 class Bag {
     constructor(_red, _green, _blue) {
         this._red = _red
+        this._green = _green
+        this._blue = _blue
     }
 
-    isPossible(red, green, blue) {
-        return this._red >= red
+    isPossibleThrow(red, green, blue) {
+        return this._red >= red && this._green >= green && this._blue >= blue
+    }
+
+    isPossibleGame(game){
+        var round = game["Round 1"]
+        return this.isPossibleThrow(round.red, round.green, round.blue)
     }
 }
 
--- Day2/Bag.test.js
+++ Day2/Bag.test.js
@@ -3,20 +3,43 @@ const { Bag } = require("./Bag.js");
 test("Empty bag, a game is not possible", () => {
     const bag = new Bag(0, 0, 0)
 
-    var possible = bag.isPossible(1, 0, 0)
+    var possible = bag.isPossibleThrow(1, 0, 0)
 
     expect(possible).toBe(false)
 })
 
-test("Bag with one ball allows a game", () => {
+test("Bag with one ball allows a throw", () => {
     const bag = new Bag(1, 0, 0)
 
-    expect(bag.isPossible(1, 0, 0)).toBe(true)
+    expect(bag.isPossibleThrow(1, 0, 0)).toBe(true)
 })
 
-test("Bag must have at least as many red balls than game", () => {
+test("Bag must have at least as many red balls as game throw", () => {
     const bag = new Bag(1, 0, 0)
 
-    expect(bag.isPossible(2, 0, 0)).toBe(false)
-    expect(bag.isPossible(1, 0, 0)).toBe(true)
+    expect(bag.isPossibleThrow(2, 0, 0)).toBe(false)
+    expect(bag.isPossibleThrow(1, 0, 0)).toBe(true)
+})
+
+test("Bag must have at least as many green balls as game throw", () => {
+    const bag = new Bag(0, 1, 0)
+
+    expect(bag.isPossibleThrow(0, 2, 0)).toBe(false)
+    expect(bag.isPossibleThrow(0, 1, 0)).toBe(true)
+})
+
+test("Bag must have at least as many blue balls as game throw", () => {
+    const bag = new Bag(0, 1, 1)
+
+    expect(bag.isPossibleThrow(0, 0, 2)).toBe(false)
+    expect(bag.isPossibleThrow(0, 0, 1)).toBe(true)
+})
+
+test("A game can have one throw", () => {
+    const bag = new Bag(0,0,1)
+    const gameA = { "Round 1": {"red": 1 , "green": 0 , "blue": 0} }
+    const gameB = { "Round 1": {"red": 0 , "green": 0 , "blue": 1} }
+
+    expect(bag.isPossibleGame(gameA)).toBe(false)
+    expect(bag.isPossibleGame(gameB)).toBe(true)
 })
