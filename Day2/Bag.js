b12c07b6ae50869d3a8b249b004c4769827c5416
jasper.jackson <jasper.jackson@madetech.com>
2024-01-02 13:47:31 +0000

    Refactored into more objects with tests on external-facing methods

--- Day2/Bag.js
+++ Day2/Bag.js
@@ -5,21 +5,98 @@ class Bag {
         this._blue = _blue
     }
 
-    isPossibleThrow(red, green, blue) {
-        return this._red >= red && this._green >= green && this._blue >= blue
-    }
-
-    isPossibleGame(game){
-        let rounds = game.length
-        for (let i = 0; i < rounds ; i++) {
-            let round = game[i]
-            if (!this.isPossibleThrow(round.red, round.green, round.blue)) {
-                return false
-            }
-        }
-
-        return true
+    toString() {
+        return `Red: ${this._red} Green: ${this._green} Blue: ${this._blue}`
     }
 }
 
-module.exports = { Bag } 
+class Conundrum {
+    constructor(bag, games) {
+        this.bag = bag
+        this.games = games
+    }
+
+    Solve() {
+        let result = 0
+        this.games.forEach(game => {
+            if (game.isPossible(this.bag)) {
+                result += game.id
+            }
+        });
+        return result
+    }
+}
+
+class Game {
+    constructor(id, rounds) {
+        this.id = id
+        this.rounds = rounds
+    }
+
+    isPossible(bag) {        
+        for (let i = 0; i < this.rounds.length ; i++) {
+            let round = this.rounds[i]
+            if (!round.isPossible(bag)) {
+                return false
+            }
+        }
+        return true
+    }
+
+    toString() {
+        let string = `Game ID: ${this.id}\n  Round count: ${this.rounds.length}`
+        for (let i = 0; i < this.rounds.length ; i++) {
+            let round = this.rounds[i]
+            string += `\n    ${round.toString()}`
+        }
+        return string
+    }
+
+    static fromString(inputText) {
+        return new Game(Game.parseIdentifier(inputText), Game.parseRounds(inputText))
+    }
+
+    static parseIdentifier(inputText) {
+        let gameIdentifier = inputText
+            .split(":")[0]
+            .substring(5)
+        return gameIdentifier
+    }
+
+    static parseRounds(inputText) {
+        let roundsList = inputText.split(":")[1]
+        let roundStrings = roundsList.split(";")
+        let rounds = roundStrings.map((roundString) => this.parseThrow(roundString))
+        return rounds
+    }
+
+    static parseThrow(throwString) {
+        let red = 0, green = 0, blue = 0
+        let colourStrings = throwString.split(",")
+        colourStrings.forEach(colourString => {
+            colourString = colourString.trim()
+            if (colourString.includes("red")) red = parseInt(colourString)
+            if (colourString.includes("green")) green = parseInt(colourString)
+            if (colourString.includes("blue")) blue = parseInt(colourString)
+        })
+        return new Round(red, green, blue)
+    }
+}
+
+class Round {
+    constructor(_red, _green, _blue) {
+        this.red = _red
+        this.green = _green
+        this.blue = _blue
+    }
+
+    toString() {
+        return `Red: ${this.red} Green: ${this.green} Blue: ${this.blue}`
+    }
+
+    isPossible(bag) {
+        return bag._red >= this.red && bag._green >= this.green && bag._blue >= this.blue
+    }
+}
+
+module.exports = { Bag, Game, Round, Conundrum } 
--- Day2/Bag.test.js
+++ Day2/Bag.test.js
@@ -1,65 +1,132 @@
-const { Bag } = require("./Bag.js");
+const { Bag, Game, Round, Conundrum} = require("./Bag.js");
 
-test("Empty bag, a game is not possible", () => {
-    const bag = new Bag(0, 0, 0)
 
-    var possible = bag.isPossibleThrow(1, 0, 0)
+describe('Testing if a round is possible', function () {
+    test('Round object can be constructed by rgb values', () => {
+        let round = new Round(1,0,99474573)
+        expect(round.red).toBe(1)
+        expect(round.green).toBe(0)
+        expect(round.blue).toBe(99474573)
+    })
+    
+    test("Empty bag, a round is not possible", () => {
+        const bag = new Bag(0, 0, 0)
+        const round = new Round(1,0,0)
 
-    expect(possible).toBe(false)
+        var possible = round.isPossible(bag)
+
+        expect(possible).toBe(false)
+    })
+
+    test("Bag with one ball allows a throw", () => {
+        const bag = new Bag(1, 0, 0)
+        const round = new Round(1,0,0)
+
+        expect(round.isPossible(bag)).toBe(true)
+    })
+
+    test("Bag must have at least as many red, green and blue balls as game throw", () => {
+        const bag = new Bag(1,1,1)
+        const round1 = new Round(2,1,1)
+        const round2 = new Round(1,2,1)
+        const round3 = new Round(1,1,2)
+        const round4 = new Round(1,1,1)
+
+        expect(round1.isPossible(bag)).toBe(false)
+        expect(round2.isPossible(bag)).toBe(false)
+        expect(round3.isPossible(bag)).toBe(false)
+        expect(round4.isPossible(bag)).toBe(true)
+    })
 })
 
-test("Bag with one ball allows a throw", () => {
-    const bag = new Bag(1, 0, 0)
 
-    expect(bag.isPossibleThrow(1, 0, 0)).toBe(true)
+describe('Testing if a game is possible', function () {
+    test("A game can be constructed with an ID and some rounds", () => {
+        const game = new Game(1, [new Round(0,0,1), new Round(1,0,1)])
+        expect(game.id).toBe(1)
+    })
+
+    test("A game can have one throw", () => {
+        const bag = new Bag(0, 0, 1)
+        const gameA = new Game(1, [new Round(1,0,0)])
+        const gameB = new Game(2, [new Round(0,0,1)])
+
+        expect(gameA.isPossible(bag)).toBe(false)
+        expect(gameB.isPossible(bag)).toBe(true)
+    })
+
+    test("A game can have several throws", () => {
+        const bag = new Bag(0, 0, 1)
+        const gameA = new Game(1, [new Round(0,0,1), new Round(1,0,1)])
+        const gameB = new Game(2, [new Round(0,0,1), new Round(0,0,1)])
+
+        expect(gameA.isPossible(bag)).toBe(false)
+        expect(gameB.isPossible(bag)).toBe(true)
+    })
 })
 
-test("Bag must have at least as many red balls as game throw", () => {
-    const bag = new Bag(1, 0, 0)
 
-    expect(bag.isPossibleThrow(2, 0, 0)).toBe(false)
-    expect(bag.isPossibleThrow(1, 0, 0)).toBe(true)
+describe('Checking overall conundrum', function () {
+    test("Return the sum of IDs of possible games", () => {
+        const smallBag = new Bag(1, 0, 1)
+        const emptyBag = new Bag(0, 0, 0)
+        const bigBag = new Bag(99, 99, 99)
+        const games = [
+            new Game(1, [
+                new Round(0,0,99)
+            ]),
+            new Game(80, [
+                new Round(0,0,1)
+            ])
+        ]
+
+        smallBagAnswer = new Conundrum(smallBag, games).Solve()
+        emptyBagAnswer = new Conundrum(emptyBag, games).Solve()
+        bigBagAnswer = new Conundrum(bigBag, games).Solve()
+
+        expect(smallBagAnswer).toBe(80)
+        expect(emptyBagAnswer).toBe(0)
+        expect(bigBagAnswer).toBe(81)
+    })
 })
 
-test("Bag must have at least as many green balls as game throw", () => {
-    const bag = new Bag(0, 1, 0)
+describe('Parsing input file row', function () {
+    /* Example input:
+    Game 9: 7 blue, 1 red; 5 red, 4 green; 4 green, 6 red, 5 blue; 2 green, 4 blue
+    Game 10: 2 red, 2 green, 2 blue; 10 blue, 2 red, 1 green; 2 green, 9 blue, 3 red
+    */
 
-    expect(bag.isPossibleThrow(0, 2, 0)).toBe(false)
-    expect(bag.isPossibleThrow(0, 1, 0)).toBe(true)
-})
+    let inputText1 = "Game 9: 7 blue, 1 red; 5 red, 4 green; 4 green, 6 red, 5 blue; 2 green, 4 blue"
+    let inputText2 = "Game 1429: 7 blue, 1 red; 5 red, 4 green; 4 green, 6 red, 5 blue"
+    // Decided no need to test for invalid inputs at this stage
 
-test("Bag must have at least as many blue balls as game throw", () => {
-    const bag = new Bag(0, 1, 1)
+    test('Game ID can be parsed from input row', () => {
+        let game1 = Game.fromString(inputText1)
+        expect(game1.id).toBe("9")
 
-    expect(bag.isPossibleThrow(0, 0, 2)).toBe(false)
-    expect(bag.isPossibleThrow(0, 0, 1)).toBe(true)
-})
+        let game2 = Game.fromString(inputText2)
+        expect(game2.id).toBe("1429")
+    })
 
-test("A game can have one throw", () => {
-    const bag = new Bag(0,0,1)
-    const gameA = [
-        {"round": 1, "red": 1 , "green": 0 , "blue": 0}
-    ]
-    const gameB = [
-        {"round": 1, "red": 0 , "green": 0 , "blue": 1}
-    ]
+    test('Correct number of games parsed from input row', () => {
+        let game1 = Game.fromString(inputText1)
+        expect(game1.rounds.length).toBe(4)
 
-    expect(bag.isPossibleGame(gameA)).toBe(false)
-    expect(bag.isPossibleGame(gameB)).toBe(true)
-})
+        let game2 = Game.fromString(inputText2)
+        expect(game2.rounds.length).toBe(3)
+    })
 
-test("A game can have several throws", () => {
-    const bag = new Bag(0,0,1)
-    const gameA = [
-        {"round": 1, "red": 0 , "green": 0 , "blue": 1},
-        {"round": 2, "red": 1 , "green": 0 , "blue": 1}
-    ]
+    test('Throw colours parsed correctly from input row', () => {
+        let game1 = Game.fromString(inputText1)
+        expect(game1.rounds[0].red).toBe(1)
+        expect(game1.rounds[2].blue).toBe(5)
 
-    const gameB = [
-        {"round": 1, "red": 0 , "green": 0 , "blue": 1},
-        {"round": 2, "red": 0 , "green": 0 , "blue": 1}
-    ]
+        let game2 = Game.fromString(inputText2)
+        expect(game2.rounds[1].green).toBe(4)
+        expect(game2.rounds[0].green).toBe(0)
+    })
 
-    expect(bag.isPossibleGame(gameA)).toBe(false)
-    expect(bag.isPossibleGame(gameB)).toBe(true)
+    // test('name', () => {
+        
+    // })
 })
