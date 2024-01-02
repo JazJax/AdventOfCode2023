e43ed3b34f4f9f1bd508f75c0c8d7feb3fe4ab89
jasper.jackson <jasper.jackson@madetech.com>
2024-01-02 17:24:13 +0000

    Stopping for the day with day1

--- Day1/code.py
+++ Day1/code.py
@@ -1,2 +1,15 @@
-def extract_number():
-    return 5
+import re
+
+def extract_number(string):
+    result = re.split(r'[a-z]+', string, flags=re.IGNORECASE)
+    first = result[0] if len(result[0]) == 1 else result[0][0]
+    last = result[-1] if len(result[-1]) == 1 else result[-1][-1]
+    return int(first + last)
+
+def load_file(filepath):
+    file = open(filepath, "r")
+    data = file.read()
+    return data
+
+def split_file(input):
+    return input.split('\n')
--- Day1/test_code.py
+++ Day1/test_code.py
@@ -1,4 +1,30 @@
 from Day1.code import *
 
-def test():
-    assert 1 == 1
+def test_extract_number():
+    string = '7a6'
+    assert extract_number('7a6') == 76
+    assert extract_number('77a6') == 76
+    assert extract_number('75a56') == 76
+    assert extract_number('077s64993jjfwjwfa6666666') == 6
+
+def test_load_file():
+    filepath = 'test_input.txt'
+    result = load_file(filepath)
+    print(result)
+    assert result == """nine92jnhgqzctpgbcbpz
+sevensddvc73three
+9986fmfqhdmq8"""
+
+def test_split_file():
+    input = """nine92jnhgqzctpgbcbpz
+sevensddvc73three
+9986fmfqhdmq8"""
+    result = split_file(input)
+    assert result[0] == 'nine92jnhgqzctpgbcbpz'
+    assert result[-1] == '9986fmfqhdmq8'
+    assert len(result) == 3
+
+def test_sum_file():
+    input = ['nine92jnhgqzctpgbcbpz','9986fmfqhdmq8']
+    assert extract_number(input[0]) == 92
+    assert extract_number(input[1]) == 98
